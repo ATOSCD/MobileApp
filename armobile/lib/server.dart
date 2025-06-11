f5246b2e9bc70458f3a5b5dba9d5657e63ff9753
KEIVEE <kewanee01@gmail.com>
2025-06-11 16:02:29 +0900

    주소 가리기

--- armobile/lib/server.dart
+++ armobile/lib/server.dart
@@ -1,3 +1,3 @@
 //const String baseUrl = '218.50.126.81:8000';
-const String baseUrl = '192.168.1.49:8000';
+const String baseUrl = '192.168.1.154:8000';
 //const String baseUrl = '10.210.1.233:8000';
